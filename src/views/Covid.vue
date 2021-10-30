2493398e1e2113ca1a7d8b25236d499dd274a852
a01653546 <A01653546@itesm.mx>
2021-10-30 11:24:24 -0500

    nEWWW

--- src/views/Covid.vue
+++ src/views/Covid.vue
@@ -1,5 +1,8 @@
 <template>
 <v-container>
+  <template>
+    <div id="map"></div>
+  </template>
   <h1 style="margin: 10px"> Covid Tracker </h1>
   <p> To search the information of a country please write the name and press enter </p>
   <p> To see more information of a country, click on the country </p>
@@ -43,12 +46,95 @@
 
 
 <script>
-
+import mapboxgl from 'mapbox-gl';
+import UnitedStates from '../components/unitedStates.geojson'
 
 export default {
 
   name: 'Covid',
   mounted() {
+      let hoveredStateId = null;
+      mapboxgl.accessToken = "pk.eyJ1IjoianVhbmRpYXpuMTIiLCJhIjoiY2t0eDJmeTRvMnBuaDJucDlqc3p0djU5bSJ9.jLvuW19MDDFc8lEnbs1xew";
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/navigation-night-v1",
+      center: [-100.562938, 38.4816921],
+      zoom: 4,
+    });
+    map.on('load', () => {
+    map.addSource('states', {
+    'type': 'geojson',
+    'data': UnitedStates
+    });
+    
+    // The feature-state dependent fill-opacity expression will render the hover effect
+    // when a feature's hover state is set to true.
+    map.addLayer({
+    'id': 'state-fills',
+    'type': 'fill',
+    'source': 'states',
+    'layout': {},
+    'paint': {
+    "fill-color": '#FF5252',
+          "fill-opacity": [
+              'case',
+              ['boolean', ['feature-state', 'hover'], false],
+              1,
+              0.75
+          ],
+          // 0.75,
+          "fill-outline-color": '#FAFAFA',
+    }
+    });
+    
+  })
+  map.on('mousemove', 'state-fills', (e) => {
+if (e.features.length > 0) {
+    console.log(e.features[0].id)
+if (hoveredStateId !== null) {
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: false }
+);
+}
+hoveredStateId = e.features[0].id;
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: true }
+);
+}
+});
+ 
+// When the mouse leaves the state-fill layer, update the feature state of the
+// previously hovered feature.
+map.on('mouseleave', 'state-fills', () => {
+if (hoveredStateId !== null) {
+map.setFeatureState(
+{ source: 'states', id: hoveredStateId },
+{ hover: false }
+);
+}
+hoveredStateId = null;
+});
+const popup = new mapboxgl.Popup({
+closeButton: false,
+closeOnClick: false,
+offset: [0, 15]
+});
+map.on('mousemove', 'state-fills', (e) => {
+// Change the cursor style as a UI indicator.
+map.getCanvas().style.cursor = 'pointer';
+// Copy coordinates array.
+const description = e.features[0].properties.NAME;
+console.log(description)
+popup.setLngLat(e.lngLat).setHTML('<div id="popper"><h1>'+description+'</h1><br><h3>Confirmados: '+e.features[0].properties.Confirmed+'</h3><br><h3>Decesos: '+e.features[0].properties.Death+'</h3></div>').addTo(map);
+popup.update();
+});
+map.on('mouseleave', 'state-fills', () => {
+map.getCanvas().style.cursor = '';
+popup.update();
+popup.remove();
+});
     this.cargarDatos()
   },
   methods:{
@@ -128,3 +214,25 @@ export default {
   })
 }
 </script>
+<style>
+#map {
+  height: 600px;
+  width: 100%;
+  margin-top: 0px ;
+}
+.mapboxgl-popup{
+    padding-bottom: 15px;
+    color:black;
+    max-width: 400px;
+    text-align: center;
+    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+}
+.text-container {
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  align-items: flex-start;
+  margin: 0 auto; /* center text container */
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,12 @@ module.exports = {
         {
           test: /\.geojson$/,
           loader: 'json-loader'
-        }
+        },
+        {
+        test: /\bmapbox-gl-csp-worker.js\b/i,
+        use: { loader: 'worker-loader' }
+        },
       ]
-    }
-  }
+  },
+}
 }
